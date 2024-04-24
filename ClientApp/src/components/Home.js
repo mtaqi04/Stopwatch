f733dee6e58caab3de57db5df3edb539253400d8
mhussaini78 <166967269+mhussaini78@users.noreply.github.com>
2024-04-24 13:30:40 -0400

    [UPDATE] - Removed an unnessecary functionality Home.js

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -3,13 +3,5 @@ import React, { Component } from 'react';
 export class Home extends Component {
   static displayName = Home.name;
 
-  render() {
-    return (
-        <div id="wrapper">
-            <div id="app"></div>
-            
-        </div>
-        
-    );
-  }
+  
 }
